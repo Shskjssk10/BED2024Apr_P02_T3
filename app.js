036b01ff18fa2deea5f74c56adb34aa90cfdf903
Hendrik Yong <hendrikyong1205@gmail.com>
2024-06-15 14:54:29 +0800

    user profile management

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 8080;
+const app = express();
+
+let users = [
+  {
+    id: 1,
+    user: "Hendrik",
+    userName: "Hendrikyong",
+    email: "test@gmail.com",
+    phoneNum: "90909090",
+    bio: "test bio",
+    gender: "male",
+    password: "test1234",
+  },
+];
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/userProfilemgmt", (req, res) => {
+  res.json(users);
+});
+
+app.get("/userProfilemgmt/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find((user) => user.id === userId);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(400).send("User not found");
+  }
+});
+
+app.put("/userProfilemgmt/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const updatedUser = req.body;
+
+  const userIndex = users.findIndex((user) => user.id === userId);
+
+  if (userId !== -1) {
+    updatedUser.id = userId;
+    users[userIndex] = updatedUser;
+    res.json(updatedUser);
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
--- public/scripts/userProfilemgmt.js
+++ public/scripts/userProfilemgmt.js
@@ -1,9 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOM loaded");
-
-  const changePfpButton = document.getElementById("change-pfp");
-
-  changePfpButton.addEventListener("click", function () {
-    console.log("Button clicked!");
-  });
 });
