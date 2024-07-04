95178ca123edd3cf56690ad67da936cf1394886f
Shskjssk10 <cadentohjunyi@gmail.com>
2024-07-04 09:19:51 +0800

    Added comment in volunterController

--- controllers/volunteerController.js
+++ controllers/volunteerController.js
@@ -44,6 +44,8 @@ const updateVolunteerProfile = async (req, res) => {
     res.status(500).send("Error updating volunteer");
   }
 };
+
+// Caden's Part
 const getAllFollowersAndFollowing = async (req, res) => {
   const id = req.params.id;
   try {
