c82d9997a03f8f292574e983472ef1b35839fb64
Shskjssk10 <cadentohjunyi@gmail.com>
2024-07-21 20:49:10 +0800

    Implemented Follower models

---
+++ models/follower.js
@@ -0,0 +1,54 @@
+const sql = require("mssql");
+const dbConfig = require("../dbConfig");
+
+class Follower {
+  constructor (Follower, FollowedBy) {
+      this.Follower = Follower;
+      this.FollowedBy = FollowedBy;
+  }
+  static async getAllFollowerRelations() {
+    const connection = await sql.connect(dbConfig);
+
+    const sqlQuery = `
+      SELECT * FROM Follower;
+    `;
+
+    const request = connection.request();
+    const result = await request.query(sqlQuery);
+
+    connection.close();
+
+    return result.recordset.map(
+      (row) =>
+        new Follower(
+          row.Follower,
+          row.FollowedBy
+        )
+    );
+  }
+  static async getFollowersByID(id){
+    const connection = await sql.connect(dbConfig);
+
+    const sqlQuery = `
+      SELECT COUNT(*) AS 'NoFollowers'
+      FROM Follower
+      WHERE Follower = @id;
+    `;
+
+    const request = connection.request();
+    request.input("id", id);
+    const result = await request.query(sqlQuery);
+
+    connection.close();
+
+    return result.recordset.map(
+      (row) =>
+        new Follower(
+          row.Follower,
+          row.FollowedBy
+        )
+    );
+  }
+}
+
+module.exports = Follower;
--- public/scripts/organisationSearch.js
+++ public/scripts/organisationSearch.js
@@ -8,51 +8,65 @@ document.addEventListener("DOMContentLoaded", async () => {
     return;
   }
   try {
+    // Getting all accounts data
     const accountResponse = await fetch("http://localhost:8080/searchPage", {
       method: "GET",
       headers: {
       "Content-Type": "application/json",
       },
-  });
-  console.log("Response status:", accountResponse.status);
-  const account = await accountResponse.json();
-  console.log("Account received:", account);
-  if (!accountResponse.ok) {
-    throw new Error(account.message || "Failed to load Account");
-  }
-  const userListSection = document.querySelector(".user-list");
-  userListSection.innerHTML = "";
+    });
+    console.log("Response status:", accountResponse.status);
+    const account = await accountResponse.json();
+    console.log("Account received:", account);
 
-  // Appending Users
-  for (const user of account){
-    const userProfileContainer = document.createElement("a");
-    userProfileContainer.classList.add("no-underline");
-    if (user.OrgName === undefined) {
-      userProfileContainer.innerHTML = `
-      <div class="user">
-        <img src="path/to/shskjsk10-profile.jpg" alt="${user.Username} profile picture" />
-        <div class="user-details">
-          <span class="username">${user.Username}</span>
-          <span class="fullname">${user.FName} ${user.LName}</span>
+    // Getting Search Query
+    const searchResponse = await fetch("http://localhost:8080/searchPage", {
+      method: "GET",
+      headers: {
+      "Content-Type": "application/json",
+      },
+    });
+    console.log("Response status:", searchResponse.status);
+    const searchedAccount = await searchResponse.json();
+    console.log("Account received:", searchedAccount);
+
+    if (!searchResponse.ok) {
+      throw new Error(searchedAccount.message || "Failed to load Searched Account");
+    }
+    const userListSection = document.querySelector(".user-list");
+    userListSection.innerHTML = "";
+
+    // Appending Users
+    let count = 0;
+    for (const user of account){
+      count += 1;
+      const userProfileContainer = document.createElement("a");
+      userProfileContainer.classList.add("no-underline");
+      if (user.OrgName === undefined) {
+        userProfileContainer.innerHTML = `
+        <div class="user">
+          <img src="path/to/shskjsk10-profile.jpg" alt="${user.Username} profile picture" />
+          <div class="user-details">
+            <span class="username">${user.Username}</span>
+            <span class="fullname">${user.FName} ${user.LName}</span>
+          </div>
+          <button class="follow-btn ${user.Username}" id="${count}">Follow</button>
         </div>
-        <button class="follow-btn">Follow</button>
-      </div>
-    `;
-    } else {
-      userProfileContainer.innerHTML = `
-      <div class="user">
-        <img src="path/to/shskjsk10-profile.jpg" alt="${user.Username} profile picture" />
-        <div class="user-details">
-          <span class="username">${user.OrgName}</span>
-          <span class="fullname">${user.Website}</span>
+      `;
+      } else {
+        userProfileContainer.innerHTML = `
+        <div class="user">
+          <img src="path/to/shskjsk10-profile.jpg" alt="${user.Username} profile picture" />
+          <div class="user-details">
+            <span class="username">${user.OrgName}</span>
+            <span class="fullname">${user.Website}</span>
+          </div>
+          <button class="follow-btn ${user.Username}" id="${count}">Follow</button>
         </div>
-        <button class="follow-btn">Follow</button>
-      </div>
-    `;
+      `;
     }
     userListSection.appendChild(userProfileContainer);
   }
-
 
   } catch (error) {
     console.error("Error loading Account:", error);
