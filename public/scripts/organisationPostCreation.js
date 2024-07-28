81d24a6f169de7eaf99cf842539bb5c3255761b3
Shskjssk10 <cadentohjunyi@gmail.com>
2024-07-28 10:59:17 +0800

    Successfully Implementedf organisationPostCreation.js

--- public/scripts/organisationPostCreation.js
+++ public/scripts/organisationPostCreation.js
@@ -8,16 +8,16 @@ document.addEventListener("DOMContentLoaded", async () => {
     return;
   }
   try {
-    const accountID = 6;
-    const imageName = "/post/imageName.jpg";
+    const accountID = localStorage.getItem("userID");
     const submittonButton = document.querySelector(".post-button");
     submittonButton.addEventListener('click', async (event) => {
       event.preventDefault();
-      console.log("Post has been clicked!!!")
+      alert("Post has been posted!")
       const caption = document.querySelector("#caption").value;
+      const randomInt = Math.floor(Math.random() * 3) + 1;
       const postPost = {
         "PostedBy" : accountID,
-        "MediaPath" : imageName,
+        "MediaPath" : `random${randomInt}-post.jpg`,
         "Caption" : caption
       }
       const postPostResponse = await fetch(
